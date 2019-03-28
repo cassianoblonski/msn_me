6e4131630d4eadce94826d4af337b31f5bf6b99f
Cassiano Blonski Sampaio <cassianobsampaio@gmail.com>
2019-03-28 10:45:01 -0300

    Add sessions create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # user = User.find_by(username: params[:session][:username].downcase)
+    # user = User.find_by(email: params[:session][:email].downcase)
 
+    user = User.find_by(email?)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+    end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to login_path
+  end
+
+  private
+    def email?
+      email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      if params[:session][:username].downcase.match(email_regex)
+        {email: params[:session][:username].downcase}
+      else
+        {username: params[:session][:username].downcase}
+      end
+    end
 end
